22f1fb3b88771762190136d94f97ba1867a1d443
trigrou <trigrou@trigrou.com>
2025-03-14 16:43:54 +0100

    Adds github actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ export default defineConfig({
     server: {
         open: true
     },
+    base: 'quantization-perf-webgl2',
     build: {
+        sourcemap: true,
+        outDir: 'dist',
+        assetsDir: 'assets',
         target: 'esnext',
         rollupOptions: {
             output: {
